f96dae635e7092ca44ab89741adc852dcc101a05
shaxzod0022 <ilgeldiyevshaxzod@gmail.com>
2026-04-08 19:19:03 +0500

    logo update

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,4 @@
 import { Lots, NewsHome, Payment, Statistica } from "@/components";
-import LotSearchBar from "@/components/lots/LotSearchBar";
 import { Suspense } from "react";
 
 export default async function Home() {
@@ -41,13 +40,6 @@ export default async function Home() {
   return (
     <main className="bg-gray-200 min-h-screen">
       <NewsHome initialData={news} />
-
-      <div className="max-w-[1900px] mx-auto 2xl:px-32 xl:px-28 lg:px-16 md:px-10 sm:px-8 px-4 py-8">
-        <Suspense fallback={<div className="h-20 bg-white/50 animate-pulse rounded-2xl" />}>
-          <LotSearchBar />
-        </Suspense>
-      </div>
-
       <Lots initialData={lots} />
       <Statistica />
       <Payment />
